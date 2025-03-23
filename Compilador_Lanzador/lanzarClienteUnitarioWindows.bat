fca7e2e10b4e6b70c1d6d1eb8e698f8eaab824f6
Moiselin69 <moisesramirezserrador@gmail.com>
2025-03-23 18:22:08 +0100

    Lanzadores actualizados

---
+++ Compilador_Lanzador/lanzarClienteUnitarioWindows.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Lanzar Cliente Unitario Windows
+echo Se lanzará el cliente unitario Auth
+java -cp "out;libs/*" sdis.broker.client.util.Auth
+
+echo Se lanzará el cliente unitario AddMsg
+java -cp "out;libs/*" sdis.broker.client.util.AddMsg
+
+echo Se lanzará el cliente unitario ReadQ"
+java -cp "out;libs/*" sdis.broker.client.util.ReadQ
+
+echo Se lanzará el cliente unitario DeleteQ
+java -cp "out;libs/*" sdis.broker.client.util.DeleteQ
+
+echo Se lanzará el cliente unitario State
+java -cp "out;libs/*" sdis.broker.client.util.State
--- Compilador_Lanzador/lanzarClienteWindows.bat
+++ Compilador_Lanzador/lanzarClienteWindows.bat
@@ -1,4 +1,4 @@
 @echo off
 REM Lanzar Cliente Windows
 cd ..
-java -cp "out;libs/*" sdis.broker.Cliente
+java -cp "out;libs/*" sdis.broker.client.Cliente
