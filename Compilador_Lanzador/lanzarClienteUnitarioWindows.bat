43953228dcfb725011c8c9eb73d693d4197c6278
Moiselin69 <moisesramirezserrador@gmail.com>
2025-03-23 19:21:18 +0100

    Quitando comas

--- Compilador_Lanzador/lanzarClienteUnitarioWindows.bat
+++ Compilador_Lanzador/lanzarClienteUnitarioWindows.bat
@@ -1,17 +1,17 @@
 @echo off
 REM Lanzar Cliente Unitario Windows
 cd ..
-echo Se lanzará el cliente unitario Auth
+echo Se lanzara el cliente unitario Auth
 java -cp "out;libs/*" sdis.broker.client.unit.Auth
 
-echo Se lanzará el cliente unitario AddMsg
+echo Se lanzara el cliente unitario AddMsg
 java -cp "out;libs/*" sdis.broker.client.unit.AddMsg
 
-echo Se lanzará el cliente unitario ReadQ"
+echo Se lanzara el cliente unitario ReadQ"
 java -cp "out;libs/*" sdis.broker.client.unit.ReadQ
 
-echo Se lanzará el cliente unitario DeleteQ
+echo Se lanzara el cliente unitario DeleteQ
 java -cp "out;libs/*" sdis.broker.client.unit.DeleteQ
 
-echo Se lanzará el cliente unitario State
+echo Se lanzara el cliente unitario State
 java -cp "out;libs/*" sdis.broker.client.unit.State
